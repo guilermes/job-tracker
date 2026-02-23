4300cae48e7cdf2acf38956defd0ed64ec7f0121
Guilherme Nobrega <guilherme.nobreg@hotmail.com>
2026-02-23 16:12:55 -0300

    updating api.ts

--- job-dashboard/src/services/api.ts
+++ job-dashboard/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { ApiResponse } from '../types/Job.ts';
 
 const api = axios.create({
-  baseURL: 'https://job-tracker-api-hj8t.onrender.com/', // URL que definimos no server.js
+  baseURL: 'https://job-tracker-api-hj8t.onrender.com', // URL que definimos no server.js
 });
 
 export const getVagas = async (cargo: string): Promise<ApiResponse> => {
